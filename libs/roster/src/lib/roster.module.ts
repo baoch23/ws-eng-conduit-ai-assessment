3adc06c259d222afce8749ff707208d58af5c095
baoch23 <baoch23.turkey@gmail.com>
2025-05-31 17:13:17 +0000

    feat: update RosterModule to include RosterService and API_URL provider

--- libs/roster/src/lib/roster.module.ts
+++ libs/roster/src/lib/roster.module.ts
@@ -1,25 +1,19 @@
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { API_URL } from '@realworld/core/http-client';
 
-@NgModule({
-  providers: [
-    { provide: API_URL, useValue: 'https://your-api-url.com' }
-  ],
-  imports: [HttpClientModule,CommonModule],
-  declarations: [RosterComponent],
-  exports: [RosterComponent],
-})
-export class RosterModule {}
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RosterComponent } from './roster.component';
 import { RouterModule } from '@angular/router';
 import { ROSTER_ROUTES } from './roster.routes';
-
+import { API_URL } from 'libs/core/http-client/src/lib/api-url.token'
+import { RosterService } from 'apps/backend/src/roster/roster.service';
 @NgModule({
   declarations: [RosterComponent],
   imports: [CommonModule, RouterModule.forChild(ROSTER_ROUTES)],
   exports: [RosterComponent],
+  providers: [
+    RosterService,
+    { provide: API_URL, useValue: 'https://your-api-url.com' }
+  ]
+
 })
 export class RosterModule {}
