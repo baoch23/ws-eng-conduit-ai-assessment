2d61a53702f57247a06feb1eec1fa4d9dcf0568d
baoch23 <baoch23.turkey@gmail.com>
2025-05-31 16:20:48 +0000

    refactor: update RosterService to use EntityRepository and improve user fetching

--- apps/backend/src/roster/roster.service.ts
+++ apps/backend/src/roster/roster.service.ts
@@ -1,43 +1,47 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { EntityManager } from '@mikro-orm/core';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { EntityRepository, MikroORM } from '@mikro-orm/core';
+import { InjectRepository } from '@mikro-orm/nestjs';
 import { User } from '../user/user.entity';
 import { Article } from '../article/article.entity';
 
 @Injectable()
 export class RosterService {
-  constructor(private readonly em: EntityManager) {}
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: EntityRepository<User>,
+    private readonly orm: MikroORM,
+  ) {}
 
   async getAll(userId?: number) {
-    if (userId) {
-      const user = await this.em.findOne(User, { id: userId }, { populate: ['followed', 'articles'] });
-      if (user) {
-        await user.articles.init();
-      }
-      if (!user) {
-        throw new NotFoundException({
-          errorCode: 404,
-          errorMsg: 'User not found.',
-        });
-      }
-      const following = await user.followed.init();
-      return following.getItems().map(f => this.mapUserToResponse(f));
-    } else {
-      const users = await this.em.find(User, {}, { populate: ['articles'] });
-      return users.map(user => this.mapUserToResponse(user)).sort((a, b) => b.favoritesCount - a.favoritesCount);
-    }
+    const users = userId ? await this.getUsersFollowedBy(userId) : await this.userRepository.findAll();
+    return this.buildRoster(users);
   }
 
-  private mapUserToResponse(user: User) {
-    const articles = user.articles.getItems();
+  private async getUsersFollowedBy(userId: number): Promise<User[]> {
+    const user = await this.userRepository.findOne({ id: userId });
+    if (!user) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+    return this.userRepository.find({ followers: user });
+  }
+
+  private async buildRoster(users: User[]): Promise<any[]> {
+    return Promise.all(users.map(user => this.buildUserRoster(user)));
+  }
+
+  private async buildUserRoster(user: User): Promise<any> {
+    const articles = await this.orm.em.find(Article, { author: user });
     const articlesCount = articles.length;
     const favoritesCount = articles.reduce((sum, article) => sum + article.favoritesCount, 0);
-    const firstArticleAt = articles.length ? articles.reduce((earliest, article) => article.createdAt < earliest ? article.createdAt : earliest, articles[0].createdAt) : '';
+    const firstArticleAt = articlesCount > 0 ? articles[0].createdAt.toISOString() : '';
+
     return {
       username: user.username,
       userId: user.id,
+      email: user.email,
       articlesCount,
       favoritesCount,
-      firstArticleAt: firstArticleAt ? firstArticleAt.toISOString() : '',
+      firstArticleAt,
     };
   }
 }
