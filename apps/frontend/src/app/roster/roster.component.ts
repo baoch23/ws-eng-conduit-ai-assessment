7eb879fe4c49f3ea1a5b47ac5dce1e54d6d63000
baoch23 <baoch23.turkey@gmail.com>
2025-05-31 17:56:31 +0000

    feat: add CommonModule and API_URL provider to RosterComponent

--- apps/frontend/src/app/roster/roster.component.ts
+++ apps/frontend/src/app/roster/roster.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 import { User } from '@realworld/core/api-types';
 import { RosterService } from './roster.service';
+import { API_URL } from 'apps/frontend/src/app/roster/roster.service';
+
 
 @Component({
   selector: 'cdt-roster',
@@ -10,11 +13,16 @@ import { RosterService } from './roster.service';
   templateUrl: './roster.component.html',
   styleUrls: ['./roster.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [CommonModule],
+  providers: [
+    RosterService,
+    { provide: API_URL, useValue: 'https://studious-journey-wr4rg656ggqvc66q-3000.app.github.dev' }
+  ]
 })
 export class RosterComponent implements OnInit {
   users$: Observable<User[]> = this.rosterService.getRoster();
 
-  constructor(private rosterService: RosterService, private store: Store) {}
+  constructor(private rosterService: RosterService, private store: Store) { }
 
   ngOnInit() {
     this.users$ = this.rosterService.getRoster();
