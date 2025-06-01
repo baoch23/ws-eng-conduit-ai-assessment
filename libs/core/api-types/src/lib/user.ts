7ef5630a1fac014d742f054365cae563943b4333
baoch23 <baoch23.turkey@gmail.com>
2025-06-01 14:34:58 +0000

    feat: add email and token fields to User interface

--- libs/core/api-types/src/lib/user.ts
+++ libs/core/api-types/src/lib/user.ts
@@ -1,7 +1,10 @@
 export interface User {
+  email: string;
+  token: string;
   username: string;
+  bio?: string | undefined;
+  image?: string | undefined;
   articlesCount: number;
   totalFavorites: number;
-  firstArticleDate: string;
-  token?: string;
+  firstArticleDate: string; // or Date, depending on your type definition
 }
